f7f25d0d10bf764e9fcc67d70486275837bb16d3
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-07-13 02:54:32 +0000

    updated logo routing

--- app/components/dashboard/Header.jsx
+++ app/components/dashboard/Header.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Moon, Sun, LogOut, Menu, X, Heading1, Router } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
